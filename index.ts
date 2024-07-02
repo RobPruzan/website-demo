bfde929bec215aa64608cf38ae0ec263fbcfec9e
RobPruzan <rapruzan@buffalo.edu>
2024-07-02 02:20:13 -0700

    upload ws server

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
+import type { ServerWebSocket } from "bun";
+
+let sockets: Array<ServerWebSocket<unknown>> = []
+
 const server = Bun.serve({
   hostname: "::",
   port: process.env.PORT ?? 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
+  fetch(req, server) {
+    server.upgrade(req);
+  
+    },
+    websocket: {
+      open: (ws) => {
+        sockets.push(ws)
+      },
+      close: (ws) => {
+        sockets = sockets.filter(s => s !== ws)
+      },
+      message: async (ws, message) => {
+        sockets.forEach(s => ws !== s && s.send(message))
+      },
+    },
 });
 
 console.log(`Listening on http://localhost:${server.port}`);
